f42ccbd67112a6cc38670d0805024e2bdfcfe25b
Sarah S <stms15@mun.ca>
2023-07-06 19:58:07 -0230

    added classes for each shape

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-const inquirer = require("inquirer");
+// const inquirer = require("inquirer");
+const Shapes = require("./lib/shapes");
+
+tri = new Shapes.Triangle("orange");
+tri.generateElement();
+console.log(tri.shape);
